93467db1aafa1b3d1d183595afea72628a55bbd2
mstoiovici <mirela.stoiovici@ada.ac.uk>
2020-01-15 12:12:29 +0000

    add constructor for Person class and test for it

--- models/person.js
+++ models/person.js
@@ -1,1 +1,10 @@
 // Your Person class goes here!
+class Person {
+    constructor(firstName, surname, dob){
+        this.firstName = firstName
+        this.surname = surname
+        this.dob = dob
+    }
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -2,3 +2,15 @@ const chai = require('chai')
 const expect = chai.expect
 
 // Your Person test goes here!
+const Person = require("../models/person.js")
+
+describe("Person", function(){
+    it("should initialize properly", function(){
+        var person = new Person("Mari", "Stoiovici", "15 June 1985")
+
+        expect(person.firstName).to.equal("Mari")
+        expect(person.surname).to.equal("Stoiovici")
+        expect(person.dob).to.equal("15 June 1985")
+    })
+
+})
